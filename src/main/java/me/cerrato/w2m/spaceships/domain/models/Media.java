35d29e155acc2081e6a4309cd5e88e681dd37c42
Alejandro C <alesagustin@gmail.com>
2024-05-24 03:50:51 +0200

    feat: added domain models

---
+++ src/main/java/me/cerrato/w2m/spaceships/domain/models/Media.java
@@ -0,0 +1,14 @@
+package me.cerrato.w2m.spaceships.domain.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Media {
+
+    private String name;
+
+    private MediaType type;
+
+}
---
+++ src/main/java/me/cerrato/w2m/spaceships/domain/models/Spaceship.java
@@ -0,0 +1,14 @@
+package me.cerrato.w2m.spaceships.domain.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Spaceship {
+
+    private String name;
+
+    private Media media;
+
+}
