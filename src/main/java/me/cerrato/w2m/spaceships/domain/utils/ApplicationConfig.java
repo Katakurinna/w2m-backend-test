bb76c721c5c1aa25b21f6d8f774e1a004d3ef481
Alejandro C <alesagustin@gmail.com>
2024-05-24 03:51:25 +0200

    feat: added config to app

---
+++ src/main/java/me/cerrato/w2m/spaceships/domain/utils/ApplicationConfig.java
@@ -0,0 +1,18 @@
+package me.cerrato.w2m.spaceships.domain.utils;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+@Configuration
+@PropertySource(value = "classpath:application.properties")
+public class ApplicationConfig {
+
+    @Value("${spaceship.database.page.size}")
+    private Integer databasePageSize;
+
+}
