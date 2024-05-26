981212b974f40a885a07b92c5ac18b644788842e
Alejandro C <alesagustin@gmail.com>
2024-05-26 09:10:03 +0200

    fix: move EnableAspectJAutoProxy annotation to application default

--- src/main/java/me/cerrato/w2m/spaceships/SpaceshipsApplication.java
+++ src/main/java/me/cerrato/w2m/spaceships/SpaceshipsApplication.java
@@ -2,8 +2,10 @@ package me.cerrato.w2m.spaceships;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class SpaceshipsApplication {
 
 	public static void main(String[] args) {
