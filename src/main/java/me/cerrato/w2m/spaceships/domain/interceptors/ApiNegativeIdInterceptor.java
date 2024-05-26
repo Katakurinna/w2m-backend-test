790976e73fb0804b9c209adff7eb7a1054ffc096
Alejandro C <alesagustin@gmail.com>
2024-05-26 05:36:16 +0200

    feat: add aspectj to log negative values

---
+++ src/main/java/me/cerrato/w2m/spaceships/domain/interceptors/ApiNegativeIdInterceptor.java
@@ -0,0 +1,23 @@
+package me.cerrato.w2m.spaceships.domain.interceptors;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+
+@Slf4j
+@Aspect
+@Component
+@EnableAspectJAutoProxy
+public class ApiNegativeIdInterceptor {
+
+    @Before(value = "execution(* me.cerrato.w2m.spaceships.api.controllers.ApiController.findById(..)) && args(id)", argNames = "id")
+    public void interceptNegativeValues(Long id) throws Throwable {
+        if (id < 0) {
+            log.error("Id is negative value {}", id);
+        }
+    }
+
+}
